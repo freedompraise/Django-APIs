661aaf789ce0b42e447d71dac95d5d610522ea5d
freedompraise <dikepraise119@gmail.com>
2022-09-26 10:54:07 +0100

    generics added

--- polls/views.py
+++ polls/views.py
@@ -1,30 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse, HttpResponse
-from rest_framework.views import APIView
+from rest_framework import generics # used for DRY 
+# It infers the response, and enables the views to be taken directly from the model class or seriaizer class
 
-from .models import Poll
+from .models import *
 from .serializers import * 
 
-def polls_list(request):
-    MAX_OBJECTS = 20
-    polls = Poll.objects.all()[:MAX_OBJECTS]
-    data = {
-        "results":list(polls.values(
-            "question","created_by__username","pub_date"
-            ))
-        }
-    return JsonResponse(data)
+class PollList(generics.ListCreateAPIView):  
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
-def polls_detail(request,pk):
-    poll = get_object_or_404(Poll, pk=pk)
-    data = {
-        "results":{
-            "question":poll.question,
-            "created_by":poll.created_by.username,
-            "pub_date":poll.pub_date
-        }
-    }
-    # TO create an polls object with serializers : 
-    # poll_serializer = PollSerializer(data={"question": "Rice or Beans", "created_ : by": 1})
-
-    return JsonResponse(data)
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+        
+        
